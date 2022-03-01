cc2aa50bc0836ad6c8b8c152b8e7ce3479f8c046
albertmartinez100 <albertmnez18@gmail.com>
2022-03-01 13:46:34 +0100

    ultima actualizacion

--- ex3.js
+++ ex3.js
@@ -3,16 +3,16 @@
 2. Asigna un valor a la variable "numero" para que se muestren TODOS los conosle.log del código. Actualmente, estamos asignando un 0.
 */
 
-let numero = 0;
+let numero = 501;
 
 if (numero > 0) {
     console.log("El número es mayor de 0.");
 }
 
 if (numero > 100) {
-    console.log("El número es mayor de 100.")
+    console.log("El número es mayor de 100.");
 }
 
 if (numero > 500) {
-    console.log("El número es mayor de 500.")
+    console.log("El número es mayor de 500.");
 }
